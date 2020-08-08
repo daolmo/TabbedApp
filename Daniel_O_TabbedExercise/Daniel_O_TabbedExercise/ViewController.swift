760157658321b3bb6b77de9b35724aaa64bc12dd
daolmo <67968940+daolmo@users.noreply.github.com>
2020-08-08 15:31:47 -0500

    First commit
    First commit

---
+++ Daniel_O_TabbedExercise/Daniel_O_TabbedExercise/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Daniel_O_TabbedExercise
+//
+//  Created by Daniel Olmo on 8/6/20.
+//  Copyright © 2020 Daniel Olmo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+
+}
+
